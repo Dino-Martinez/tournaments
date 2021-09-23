301931b07f27db9d3b77f53aab551f75b4ae06ce
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-09-23 00:00:03 -0400

    [Update] index.js test.js
    Sample of checking for auth in a fullstack workflow

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,4 +1,8 @@
-export default function handler (req, res) {
+import { getSession } from 'next-auth/client'
+
+export default async function handler (req, res) {
+  const session = await getSession({ req })
+  console.log(session)
   if (req.method === 'GET') {
     res.status(200).json({ result: 'Hello!' })
   }

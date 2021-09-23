2b54d1d9ec8ca12197588c4b3a207d63cd6260b0
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-09-23 02:37:18 -0400

    Implemented site-wide Layout component for navigation and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import { useSession } from 'next-auth/client'
+import Layout from '../components/Layout'
 
 function MyApp ({ Component, pageProps }) {
   const [session, loading] = useSession()
-  return <Component {...pageProps} session={session} authenticating={loading}/>
+  return (
+    <Layout session={session}>
+      <Component {...pageProps} session={session} authenticating={loading}/>
+    </Layout>
+  )
 }
 
 export default MyApp

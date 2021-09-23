b233f6ec0bde9eaca934e2a45a26027b9d4ff0a8
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-09-23 19:44:56 -0400

    [Update] components/ProfileData
    Added input form to profile page

--- components/ProfileData.js
+++ components/ProfileData.js
@@ -1,8 +1,30 @@
+import InputForm from './InputForm'
+
+const fields = [
+  {
+    title: 'username',
+    type: 'text'
+  },
+  {
+    title: 'bio',
+    type: 'text'
+  },
+  {
+    title: 'age',
+    type: 'number'
+  }
+]
+
+const onSubmit = (values) => { console.log(values) }
+
 export default function ProfileData ({ data }) {
   return (
-  <ul>
-    <li>{data.email}</li>
-    <li>{data.name}</li>
-  </ul>
+    <>
+      <ul>
+        <li>{data.email}</li>
+        <li>{data.name}</li>
+      </ul>
+      <InputForm fields={fields} onSubmit={onSubmit}></InputForm>
+    </>
   )
 }

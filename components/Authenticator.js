c55b073ed0d5024db877025dc1b2489591c283df
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-09-24 02:13:33 -0400

    [Update] Authenticator.js
    Added the sign in form to the authenticator

--- components/Authenticator.js
+++ components/Authenticator.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-
+import SignInForm from './signinForm'
 export default function Authenticator ({ setReady, session, authenticating }) {
   useEffect(() => { if (session) setReady(true) }, [session])
   return (
@@ -9,6 +9,9 @@ export default function Authenticator ({ setReady, session, authenticating }) {
           <h1>Authenticating session...</h1>
           <div className="lds-dual-ring"></div>
         </div>
+      }
+      {!authenticating && !session &&
+        <SignInForm session={session}></SignInForm>
       }
     </>
   )

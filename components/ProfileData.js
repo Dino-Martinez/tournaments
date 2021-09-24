70b6a90efde09f844e2f1bd6dd0338dce835ada2
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-09-24 02:17:15 -0400

    [Update] ProfileData
    Composed profile data of DataList and InputForm components

--- components/ProfileData.js
+++ components/ProfileData.js
@@ -1,29 +1,54 @@
+import { useEffect } from 'react'
+import useApi from '../hooks/useApi'
 import InputForm from './InputForm'
+import DataList from './DataList'
 
+// This is the shape of our input form for updating a user profile
 const fields = [
   {
-    title: 'username',
-    type: 'text'
+    key: 'username',
+    type: 'text',
+    label: 'Username'
   },
   {
-    title: 'bio',
-    type: 'text'
+    key: 'bio',
+    type: 'text',
+    label: 'Biography'
   },
   {
-    title: 'age',
-    type: 'number'
+    key: 'age',
+    type: 'number',
+    label: 'Age'
   }
 ]
 
-const onSubmit = (values) => { console.log(values) }
+// This is the shape of the displayed data, including immutable data
+const shape = [...fields,
+  {
+    key: 'email',
+    label: 'Email'
+  },
+  {
+    key: 'name',
+    label: 'Full Name'
+  }
+]
 
-export default function ProfileData ({ data }) {
+export default function ProfileData ({ data, refresh, session }) {
+  const [update, refetch, loading] = useApi('/api/users')
+  const onSubmit = (values) => {
+    console.log(values)
+    refetch(session.user.email, { method: 'PUT', body: JSON.stringify(values) })
+  }
+
+  useEffect(() => {
+    if (update && !loading) {
+      refresh()
+    }
+  }, [update, loading])
   return (
     <>
-      <ul>
-        <li>{data.email}</li>
-        <li>{data.name}</li>
-      </ul>
+      <DataList data={data} shape={shape}/>
       <InputForm fields={fields} onSubmit={onSubmit}></InputForm>
     </>
   )

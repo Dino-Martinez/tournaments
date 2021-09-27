e4221f045afc3ca77258d6f20d79b6a8c2a3b4c6
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-09-27 01:51:43 -0400

    [Update] TournamentData
    Added list of registered users

--- components/TournamentData.js
+++ components/TournamentData.js
@@ -1,5 +1,17 @@
-export default function TournamentData ({ data }) {
+import Link from 'next/link'
+import generateKey from '../lib/generateKey'
+export default function TournamentData ({ data, tid }) {
+  const keys = generateKey()
   return (
-    <p>{data.title}</p>
+    <>
+      <h2>{data.title}</h2>
+      <p>{data.description}</p>
+      {data.registered &&
+        <ul>
+          {data.registered.map(participant => <li key={keys.next().value}>{participant}</li>)}
+        </ul>
+      }
+      <Link href={`/tournaments/${tid}/register`}>Register</Link>
+    </>
   )
 }

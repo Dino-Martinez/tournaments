1540b552023ca728ced5c2ebeb9f9eb8be046baa
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-05 00:58:50 -0400

    [Add] api/games
    Implemented back end routes to access list of games

---
+++ pages/api/games/[name].js
@@ -0,0 +1,14 @@
+import { connectToDatabase } from '../../../../lib/mongodb'
+import { ObjectId } from 'mongodb'
+
+export default async function handler (req, res) {
+  const { name } = req.query
+  const { db } = await connectToDatabase()
+
+  if (req.method === 'GET') {
+    const game = await db.collection('game')
+      .findOne({ _id: new ObjectId(name) })
+
+    return res.status(200).json(game)
+  }
+}

a009b725101e11ddc13bca1789e5715a981051f6
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-05 01:22:18 -0400

    [Update] api/games/[name]
    Fixed import statement

--- pages/api/games/[name].js
+++ pages/api/games/[name].js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from '../../../../lib/mongodb'
+import { connectToDatabase } from '../../../lib/mongodb'
 import { ObjectId } from 'mongodb'
 
 export default async function handler (req, res) {

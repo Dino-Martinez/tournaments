cd61df23be8b1035f9abed6b7c506e8198000d64
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-09 21:58:19 -0400

    [Update] teams/create
    Implemented authentication protection

--- pages/teams/create.js
+++ pages/teams/create.js
@@ -1,5 +1,38 @@
+import { useContext, useState } from 'react'
+import InputForm from '../../components/InputForm'
+import { AuthContext } from '../../hooks/useAuth'
+import Authenticator from '../../components/Authenticator'
+
+const fields = [
+  {
+    key: 'name',
+    type: 'text',
+    label: 'Team Name'
+  },
+  {
+    key: 'authorIsPlayer',
+    type: 'checkbox',
+    label: 'Act as a player?'
+  }
+]
 export default function CreateTeam () {
+  const [session] = useContext(AuthContext)
+  const [authenticated, setAuthenticatedStatus] = useState(false)
+
+  const onSubmit = (values) => {
+    values.author = session.user.email
+    console.log(values)
+  }
+
   return (
-    <h1>Create a team</h1>
+    <>
+      <Authenticator setReady={setAuthenticatedStatus}/>
+      {authenticated &&
+      <>
+        <h1>Create a team:</h1>
+        <InputForm fields={fields} onSubmit={onSubmit} />
+      </>
+      }
+    </>
   )
 }

50a2921897a591e42fbfb6a3eebced3ce9d4ddeb
Tristan Thompson <tristan15thompson@gmail.com>
2021-10-14 16:15:54 -0400

    add: dynamic bracket size options on bracket page

--- pages/tournaments/[tid]/bracket.js
+++ pages/tournaments/[tid]/bracket.js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import ApiResolver from '../../../components/ApiResolver'
 import useApi from '../../../hooks/useApi'
 import { Bracket } from 'react-brackets'
+import { useEffect, useState } from 'react'
 
 export async function getServerSideProps (context) {
   return {
@@ -13,66 +14,21 @@ export default function Tournament () {
   const router = useRouter()
   const { tid } = router.query
   const [data, loading] = useApi(`/api/tournaments/${tid}`, {}, [], true)
+  const [rounds, setRounds] = useState([])
 
-  const rounds = [
-    {
-      title: 'Round one',
-      seeds: [
-        {
-          id: 1,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team A' }, { name: 'Team B' }]
-        },
-        {
-          id: 2,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team C' }, { name: 'Team D' }]
-        },
-        {
-          id: 3,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team E' }, { name: 'Team F' }]
-        },
-        {
-          id: 4,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team G' }, { name: 'Team H' }]
-        }
-      ]
-    },
-    {
-      title: 'Round two',
-      seeds: [
-        {
-          id: 3,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team A' }, { name: 'Team C' }]
-        },
-        {
-          id: 3,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team A' }, { name: 'Team C' }]
-        }
-      ]
-    },
-    {
-      title: 'Round two',
-      seeds: [
-        {
-          id: 3,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team A' }, { name: 'Team C' }]
-        }
-      ]
+  useEffect(() => {
+    if (data && !loading) {
+      setRounds(data.rounds)
+      console.log(data.tournaments)
     }
-  ]
+  }, [data, loading])
 
   return (
     <ApiResolver data={data} loading={loading}>
-      <Bracket rounds={rounds} />
       <button type="button" onClick={() => router.push(`/tournaments/${tid}`)}>
         Back
       </button>
+      <Bracket rounds={rounds} />
     </ApiResolver>
   )
 }

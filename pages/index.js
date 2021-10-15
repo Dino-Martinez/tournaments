37b1a2a4b46c8547932126ed066dc48201c1dfe2
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-15 17:08:23 -0400

    [Update] index.js
    Added github link to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home () {
   return (
     <div>
-      <h1>Landing page</h1>
+      <h1>Tournaments</h1>
+      <p>Check out our github for more information</p>
+      <a href="https://github.com/Dino-Martinez/tournaments">GitHub</a>
     </div>
   )
 }

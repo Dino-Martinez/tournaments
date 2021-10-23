2482b61e896b6aefbbfca869eecde4b71c32a150
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-23 03:34:14 -0400

    [Update] SigninForm
    Added a callback url to signin and sign out functions

--- components/signinForm.js
+++ components/signinForm.js
@@ -2,21 +2,21 @@ import { signIn, signOut } from 'next-auth/client'
 import { useContext } from 'react'
 import { AuthContext } from '../hooks/useAuth'
 
-export default function SignInForm () {
+export default function SignInForm ({ redirect = '/' }) {
   const [session] = useContext(AuthContext)
   return (
     <article>
       {!session &&
         <>
           <h2>You must be signed in to use this page.</h2>
-          <button onClick={() => signIn('google')}>Sign in</button>
+          <button onClick={() => signIn('google', { callbackUrl: redirect })}>Sign in</button>
           <p>Sign in with Google</p>
         </>
       }
       {session &&
         <>
           <p> { session.user.name || session.user.email }</p>
-          <button onClick={() => signOut()}>Sign out</button>
+          <button onClick={() => signOut({ callbackUrl: '/' })}>Sign out</button>
         </>
       }
     </article>

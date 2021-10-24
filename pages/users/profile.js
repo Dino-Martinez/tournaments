b791e609891d0d9719f3f7b342ce540a497b7bf2
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-23 21:12:52 -0400

    [Update] users/profile
    Included an auth property to handle route protection

--- pages/users/profile.js
+++ pages/users/profile.js
@@ -1,25 +1,19 @@
-import { useContext, useEffect, useState } from 'react'
 import useApi from '../../hooks/useApi'
-import Authenticator from '../../components/Authenticator'
 import ApiResolver from '../../components/ApiResolver'
 import ProfileData from '../../components/ProfileData'
-import { AuthContext } from '../../hooks/useAuth'
 
 export default function Profile () {
-  const [session] = useContext(AuthContext)
-  const { data, loading, refetch } = useApi('/api/users')
-  const [authenticated, setAuthenticatedStatus] = useState(false)
-
-  const refresh = () => { if (authenticated) refetch(session.user.email) }
-
-  useEffect(() => { refresh() }, [authenticated])
-
+  const { data, loading } = useApi('/api/users')
   return (
     <>
-      <Authenticator setReady={setAuthenticatedStatus} />
       <ApiResolver loading={loading} data={data}>
-        <ProfileData data={data} refresh={refresh}></ProfileData>
+        <ProfileData data={data}></ProfileData>
       </ApiResolver>
     </>
   )
+}
+
+Profile.auth = {
+  protected: true,
+  callbackUrl: '/profile'
 }

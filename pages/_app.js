8f16bb0493bae3052259958f49a81d7c16a0d81a
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-23 20:38:19 -0400

    [Update] _app
    Swapped custom session provider for next-auth session provider. Conditionally authneticate routes based on their component .auth values

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,32 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-import AuthProvider from '../hooks/useAuth'
+import Authenticator from '../components/Authenticator'
 import Head from 'next/head'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp ({ Component, pageProps }) {
+function MyApp ({
+  Component,
+  pageProps: { session, ...pageProps }
+}) {
   return (
-    <AuthProvider>
+    <SessionProvider session={session}>
       <Layout>
         <Head>
           <title>Tournament Site</title>
           <meta name="description" content="A webapp to find tournaments" />
         </Head>
-        <Component {...pageProps}/>
+
+        {Component.auth
+          ? (
+            <Authenticator>
+              <Component {...pageProps}/>
+            </Authenticator>
+          )
+          : (
+            <Component {...pageProps}/>
+          )}
       </Layout>
-    </AuthProvider>
+    </SessionProvider>
   )
 }
 

b8b9b8ec6f572fe3524fcc0b6cb5195b647225fe
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-23 22:03:32 -0400

    [Update] users/profile
    Abstracted fetching and data handling to profiledata component

--- pages/users/profile.js
+++ pages/users/profile.js
@@ -1,15 +1,8 @@
-import useApi from '../../hooks/useApi'
-import ApiResolver from '../../components/ApiResolver'
 import ProfileData from '../../components/ProfileData'
 
 export default function Profile () {
-  const { data, loading } = useApi('/api/users')
   return (
-    <>
-      <ApiResolver loading={loading} data={data}>
-        <ProfileData data={data}></ProfileData>
-      </ApiResolver>
-    </>
+    <ProfileData></ProfileData>
   )
 }
 

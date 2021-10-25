78a82dc78db6ab6dc5042d94ca2e0c1fa29c7b63
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-10-24 22:49:09 -0400

    [Update] BackButton
    Fixed React out of scope to resolve conflict with new branch

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router'
 import styles from '../styles/backbutton.module.css'
+import React from 'react'
+
 export default function BackButton () {
   const router = useRouter()
 

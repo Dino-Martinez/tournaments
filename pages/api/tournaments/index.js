f56444e0d05ccdc593decb48061038d6c129cdaa
Dino-Martinez <Dino.Martinez.514@gmail.com>
2022-01-11 15:17:27 -0500

    [Update] api/tournaments/index.js
    Implemented a route to receive all tournaments within a given category

--- pages/api/tournaments/index.js
+++ pages/api/tournaments/index.js
@@ -1,10 +1,15 @@
 import Tournament from '../../../models/Tournament'
+import Game from '../../../models/Game'
 import connectDB from '../../../lib/db'
 
 const handler = async (req, res) => {
   if (req.method === 'GET') {
+    const query = req.query.category.slice(0, -1)
+    const category = await Game
+      .findOne({ name: query })
+      .lean()
     const tournaments = await Tournament
-      .find()
+      .find({ game: category._id })
       .populate('registered')
       .lean()
 
@@ -13,6 +18,12 @@ const handler = async (req, res) => {
 
   if (req.method === 'POST') {
     const tournament = new Tournament(req.body)
+    const game = await Game
+      .find({ name: tournament.game })
+      .lean()
+
+    game.upcoming.push(tournament)
+    await game.save()
 
     const result = await tournament.save()
 

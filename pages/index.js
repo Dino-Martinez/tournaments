66213c36570fb694d99333225d36c2f16cd3496a
Dino-Martinez <Dino.Martinez.514@gmail.com>
2022-01-11 15:18:07 -0500

    [Update] index.js tournaments/index.js
    Implemented frontend to view all categories and to view tournaments within a category

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import CategoryList from '../components/CategoryList'
 
 export default function Home () {
   return (
     <div>
       <h1>Tournaments</h1>
-      <p>Check out our github for more information</p>
-      <a href="https://github.com/Dino-Martinez/tournaments">GitHub</a>
+      <CategoryList></CategoryList>
     </div>
   )
 }
